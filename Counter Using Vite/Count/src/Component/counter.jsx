a8cba277f2fd395ce40b6efa138f1a86191f2f79
Bhumit Jain <bhumitjain942@gmail.com>
2024-02-07 15:58:36 +0530

    counter button using vite

---
+++ Counter Using Vite/Count/src/Component/counter.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './Counter.css';
+
+export function Counter() {
+  const [count, setCount] = useState(1);
+
+  const handleIncrease = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrease = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="counter-container">
+      <p className="count">{count}</p>
+      <button className="increase-btn" onClick={handleIncrease}>
+        Increase
+      </button>
+      <button className="decrease-btn" onClick={handleDecrease}>
+        Decrease
+      </button>
+    </div>
+  );
+}
